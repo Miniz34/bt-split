cffa02eec1d132d5554a6da99e30aa09508bc810
Karim Farhi <ka.farhi@gmail.com>
2023-10-28 23:43:37 +0200

    add error handling

--- pages/components/PlayerCard.tsx
+++ pages/components/PlayerCard.tsx
@@ -18,28 +18,32 @@ function PlayerCard({ players }: any) {
   return (
     <>
       <div className={styles.card}>
-        {players.map((player: any) => (
-          <div key={player._id} className={styles.carddata}>
-            <p>Name: {player.name}</p>
-            <p>Class: {player.class}</p>
-            <p>token: {player.token}</p>
-            <div>
-              <p>Set:</p>
-              <ul>
-                {Object.keys(player.set).map((setItem) => (
-                  <li
-                    key={setItem}
-                    className={getSetItemClassName(player.set[setItem])}
-                  >
-                    {setItem}: {player.set[setItem]}
-                  </li>
-                ))}
-              </ul>
+        {players && Array.isArray(players) ? (
+          players.map((player: any) => (
+            <div key={player._id} className={styles.carddata}>
+              <p>Name: {player.name}</p>
+              <p>Class: {player.class}</p>
+              <p>token: {player.token}</p>
+              <div>
+                <p>Set:</p>
+                <ul>
+                  {Object.keys(player.set).map((setItem) => (
+                    <li
+                      key={setItem}
+                      className={getSetItemClassName(player.set[setItem])}
+                    >
+                      {setItem}: {player.set[setItem]}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <p>beuteu: {player.beuteu}</p>
             </div>
-
-            <p>beuteu: {player.beuteu}</p>
-          </div>
-        ))}
+          ))
+        ) : (
+          // Handle the case when players is not an array
+          <p>Invalid players data</p>
+        )}
       </div>
     </>
   );
